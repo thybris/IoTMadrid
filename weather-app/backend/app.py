7036bf0643cae9cbcd9eea5e18d15d625d5e86ff
Julotrocho <65762607+Julotrocho@users.noreply.github.com>
2023-12-10 18:57:16 +0100

    adding censor to the web app part, still need to implement them

--- weather-app/backend/app.py
+++ weather-app/backend/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import random
 
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
 API_KEY = 'eadcae6451abb11beffed9dc638dcac3'
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast"
+
+def get_sensor_data():
+    temperature = 24  # round(random.uniform(20, 30), 2)
+    humidity = 12  # round(random.uniform(40, 60), 2)
+    return {'temperature': temperature, 'humidity': humidity}
 
 @app.route('/')
 def index():
@@ -21,6 +27,11 @@ def get_weather():
     }
     response = requests.get(WEATHER_URL, params=params)
     return jsonify(response.json())
+
+@app.route('/get_sensor_data', methods=['GET'])
+def get_sensor_data_route():
+    sensor_data = get_sensor_data()
+    return jsonify(sensor_data)
 
 @app.route('/get_forecast', methods=['GET'])
 def get_forecast():
--- weather-app/frontend/static/js/script.js
+++ weather-app/frontend/static/js/script.js
@@ -1,23 +1,42 @@
-document.getElementById('weatherForm').addEventListener('submit', function(event){
+document.getElementById('weatherForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var city = document.getElementById('cityInput').value;
+
+    // Fetch weather data
     fetch('/get_weather?city=' + city)
         .then(response => response.json())
         .then(data => {
             displayWeatherData(data);
-            getForecast(city);
         })
         .catch(error => {
-            console.error('Error:', error);
+            console.error('Error fetching weather data:', error);
             document.getElementById('weatherData').innerText = 'Failed to load weather data';
         });
+
+    // Fetch sensor data
+    fetch('/get_sensor_data')
+        .then(response => response.json())
+        .then(sensorData => {
+            renderSensorData(sensorData);
+        })
+        .catch(error => {
+            console.error('Error fetching sensor data:', error);
+            // Handle the error as needed
+        });
+
+    // Fetch forecast data
+    getForecast(city);
 });
 
 function displayWeatherData(data) {
+    console.log('Weather Data:', data);  // Log the received data
+
     if (data.cod !== 200) {
         document.getElementById('weatherData').innerText = 'Weather data not found';
         return;
     }
+
+    // Rest of the function remains the same
     document.getElementById('weatherLocation').textContent = `Weather in ${data.name}, ${data.sys.country}`;
     document.getElementById('weatherDescription').textContent = `${data.weather[0].main} (${data.weather[0].description})`;
     document.getElementById('weatherTemperature').textContent = `Temperature: ${data.main.temp.toFixed(2)}°C`;
@@ -25,19 +44,27 @@ function displayWeatherData(data) {
     document.getElementById('weatherWind').textContent = `Wind Speed: ${data.wind.speed} m/s`;
 }
 
+
+function renderSensorData(sensorData) {
+    document.getElementById('sensorTemperature').textContent = `Temperature: ${sensorData.temperature} °C`;
+    document.getElementById('sensorHumidity').textContent = `Humidity: ${sensorData.humidity} %`;
+    // Update other sensor data fields as needed
+}
+
 function getForecast(city) {
+    // Fetch forecast data
     fetch('/get_forecast?city=' + city)
         .then(response => response.json())
         .then(data => {
             renderForecastChart(data);
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error fetching forecast data:', error));
 }
 
 function renderForecastChart(forecastData) {
     const labels = forecastData.list.map(item => new Date(item.dt * 1000).toLocaleTimeString());
     const temps = forecastData.list.map(item => item.main.temp);
-    
+
     const ctx = document.getElementById('forecastChart').getContext('2d');
     if (window.myForecastChart) {
         window.myForecastChart.destroy();
